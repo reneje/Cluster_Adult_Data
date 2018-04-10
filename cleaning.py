ffbc7a6449f348bdd4f78a76451584030d2158c9
reneje <36469891+reneje@users.noreply.github.com>
2018-04-10 19:53:46 +0700

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -11,12 +11,15 @@ print(type(files))
 index = []
 for i in range (0,len(files)):
     m = re.search(r"[\?]",files[i])
-    if m is not None:
-        index.append(i)
+#     if m is not None:
+#         index.append(i)
+      if m is None:
+          fileopen.writelines(files[i])
+          fileopen.writelines("\n")
 
-for i in range (0,len(files)):
-    if i not in index:
-        fileopen.writelines(files[i])
-        fileopen.writelines("\n")
+# for i in range (0,len(files)):
+#     if i not in index:
+#         fileopen.writelines(files[i])
+#         fileopen.writelines("\n")
 
 fileopen.close()
