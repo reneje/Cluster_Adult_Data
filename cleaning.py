0e7c11061532dbf03ae5e9da62ae5b6280f156c6
reneje <36469891+reneje@users.noreply.github.com>
2018-04-10 19:54:24 +0700

    cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -8,15 +8,18 @@ fileopen = open("adulttest.csv","w+")
 files = open(filename).read()
 files = files.split("\n")
 print(type(files))
-index = []
+# index = []
 for i in range (0,len(files)):
     m = re.search(r"[\?]",files[i])
-    if m is not None:
-        index.append(i)
+#     if m is not None:
+#         index.append(i)
+      if m is None:
+          fileopen.writelines(files[i])
+          fileopen.writelines("\n")
 
-for i in range (0,len(files)):
-    if i not in index:
-        fileopen.writelines(files[i])
-        fileopen.writelines("\n")
+# for i in range (0,len(files)):
+#     if i not in index:
+#         fileopen.writelines(files[i])
+#         fileopen.writelines("\n")
 
 fileopen.close()
