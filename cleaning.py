f913d33c436eedb28212bcb0231b168fbf121a2c
Meisaputri <mei.silviana93@gmail.com>
2018-04-10 14:53:49 +0700

    upload cleansing

---
+++ cleaning.py
@@ -0,0 +1,22 @@
+import re
+
+# filename = 'adult.data'
+# fileopen = open("adultdata.csv","w+")
+filename = 'adult.test'
+fileopen = open("adulttest.csv","w+")
+
+files = open(filename).read()
+files = files.split("\n")
+print(type(files))
+index = []
+for i in range (0,len(files)):
+    m = re.search(r"[\?]",files[i])
+    if m is not None:
+        index.append(i)
+
+for i in range (0,len(files)):
+    if i not in index:
+        fileopen.writelines(files[i])
+        fileopen.writelines("\n")
+
+fileopen.close()
